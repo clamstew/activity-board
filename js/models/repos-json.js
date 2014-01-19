c773c095c352fafe31f5ea34b7ef42539019123f
Clay Stewart <claytonmstewart@gmail.com>
2014-01-19 13:52:47 -0600

    Convert main json repo list to an array of objects and change the for loop on the main page to work with that accordingly

--- js/models/repos-json.js
+++ js/models/repos-json.js
@@ -1,46 +1,46 @@
-window.repos = {
-	0: {
-		user: "GeneChatham",
-		repo: "mmm"
-	},
-	1: {
-		user: "LesleyMdCampo",
-		repo: "Point_B"
-	},
-	2: {
-		user: "amhayslip",
-		repo: "goforit"
-	},
-	3: {
-		user: "krytlewski",
-		repo: "remory"
-	},
-	4: {
-		user: "RollWave07",
-		repo: "CareWell"
-	},
-	5: {
-		user: "caldwelljs",
-		repo: "makerpong"
-	},
-	6: {
-		user: "gpfeff",
-		repo: "FanFollow-2.0"
-	},
-	7: {
-		user: "erlaroche",
-		repo: "Home_Sit"
-	},
-	8: {
-		user: "qualquervalor",
-		repo: "we-share"
-	},
-	9: {
-		user: "bird-man-jr",
-		repo: "local_farm_roots"
-	},
-	10: {
-		user: "bensbigolbeard",
-		repo: "marsattracts"
-	}
-};
+window.repos = [
+    {
+        user: "GeneChatham",
+        repo: "mmm"
+    },
+    {
+        user: "LesleyMdCampo",
+        repo: "Point_B"
+    },
+    {
+        user: "amhayslip",
+        repo: "goforit"
+    },
+    {
+        user: "krytlewski",
+        repo: "remory"
+    },
+    {
+        user: "RollWave07",
+        repo: "CareWell"
+    },
+    {
+        user: "caldwelljs",
+        repo: "makerpong"
+    },
+    {
+        user: "gpfeff",
+        repo: "FanFollow-2.0"
+    },
+    {
+        user: "erlaroche",
+        repo: "Home_Sit"
+    },
+    {
+        user: "qualquervalor",
+        repo: "we-share"
+    },
+    {
+        user: "bird-man-jr",
+        repo: "local_farm_roots"
+    },
+    {
+        user: "bensbigolbeard",
+        repo: "marsattracts"
+    }
+];
