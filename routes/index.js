76c73e12c4d17101bec842b78aa94c3d219117b7
Clay Stewart <cstewart@rosettastone.com>
2014-11-13 10:05:16 -0600

    Add asset viewer route to routes

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,22 @@ module.exports = function(router) {
   router.get('/', function(req, res) {
     res.render('index');
   });
+
+  router.get('/assets', function(req, res) {
+    console.log('is this happening??');
+    var walk    = require('walk');
+    var count = 0;
+
+    // Walker options
+    var walker  = walk.walk('./public', { followLinks: false });
+
+    // print out the public directory to console
+    walker.on('directory', function (path, stat, next) {
+      count += 1;
+      console.log( [path, '/', stat.name].join('') )
+      next();
+    });
+
+  });
+
 }
