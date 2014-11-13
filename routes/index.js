0ed2f8d89aeaa37f1dc60a65139e6014d0cc90d5
Clay Stewart <cstewart@rosettastone.com>
2014-11-13 13:14:08 -0600

    remove assets route comment out for time being

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,29 @@
 
 module.exports = function(router) {
   router.get('/', function(req, res) {
+    console.log('rendering the homepage ..');
     res.render('index');
   });
 
-  router.get('/assets', function(req, res) {
-    console.log('is this happening??');
-    var walk    = require('walk');
-    var count = 0;
+  // router.get('/assets', function(req, res) {
+  //   console.log('is this happening??');
+  //   var walk    = require('walk');
+  //   var count = 0;
 
-    // Walker options
-    var walker  = walk.walk('.tmp/', { followLinks: false });
+  //   // Walker options
+  //   var walker  = walk.walk('.tmp/', { followLinks: false });
 
-    // print out the public directory to console
-    walker.on('directory', function (path, stat, next) {
-      count += 1;
-      console.log( [path, '/', stat.name].join('') )
-      next();
-    });
+  //   // print out the public directory to console
+  //   walker.on('directory', function (path, stat, next) {
+  //     count += 1;
+  //     console.log( [path, '/', stat.name].join('') )
+  //     next();
+  //   });
 
-    walker.on('end', function() {
-        console.log('done walking...');
-    });
+  //   walker.on('end', function() {
+  //       console.log('done walking...');
+  //   });
 
-  });
+  // });
 
 }
