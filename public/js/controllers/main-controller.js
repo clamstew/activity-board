c996b9930ae67826567df3228fc9ae7e3492f266
Clay Stewart <cstewart@rosettastone.com>
2014-11-13 15:01:06 -0600

    simplify toggle sidebar functions

--- public/js/controllers/main-controller.js
+++ public/js/controllers/main-controller.js
@@ -12,18 +12,12 @@
       if (angular.element('aside').css('display') == 'none') {
         angular.element('aside').css('display', 'block');
         angular.element('.aside-open').css('display', 'none');
-      } else {
-        angular.element('aside').css('display', 'none');
-        angular.element('.aside-open').css('display', 'block');
       }
     };
 
     $scope.hideSidebar = function() {
-      console.log('toggling showSidebar ...');
-      if (angular.element('aside').css('display') == 'none') {
-        angular.element('aside').css('display', 'block');
-        angular.element('.aside-open').css('display', 'none');
-      } else {
+      console.log('toggling hideSidebar ...');
+      if (angular.element('aside').css('display') == 'block') {
         angular.element('aside').css('display', 'none');
         angular.element('.aside-open').css('display', 'block');
       }
