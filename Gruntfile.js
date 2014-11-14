cf7d1cf78f0da552313456a94e2699cb86529a7b
Clay Stewart <cstewart@rosettastone.com>
2014-11-13 23:31:14 -0600

    reformat grunt file spacing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 'use strict';
 
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
-    grunt.initConfig({
-        sass: {
-            options: {
-                sourceMap: true
-            },
-            dist: {
-                files: {
-                    'public/style/main.css': 'public/style/main.sass'
-                }
-            }
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'public/style/main.css': 'public/style/main.sass'
         }
-    });
+      }
+    }
+  });
 
-    grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass']);
 };
