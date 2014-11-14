ab3dede3765fcaa45ba84a63c4b9cc2f43461c08
Clay Stewart <cstewart@rosettastone.com>
2014-11-14 00:10:09 -0600

    Move scope show hide sidebar scope functions to directive

--- public/js/controllers/main-controller.js
+++ public/js/controllers/main-controller.js
@@ -5,23 +5,7 @@
   app.controller('MainCtrl', ['$scope', 'repos', function($scope, repos){
 
     $scope.repos = repos;
-    console.log('$scope.repos', $scope.repos);
-
-    $scope.showSidebar = function() {
-      console.log('toggling showSidebar ...');
-      if (angular.element('aside').css('display') == 'none') {
-        angular.element('aside').css('display', 'block');
-        angular.element('.aside-open').css('display', 'none');
-      }
-    };
-
-    $scope.hideSidebar = function() {
-      console.log('toggling hideSidebar ...');
-      if (angular.element('aside').css('display') == 'block') {
-        angular.element('aside').css('display', 'none');
-        angular.element('.aside-open').css('display', 'block');
-      }
-    };
+    // console.log('$scope.repos', $scope.repos);
 
   }]);
 
--- public/js/directives/sidebar.js
+++ public/js/directives/sidebar.js
@@ -3,9 +3,30 @@
 (function(app) {
 
   app.directive('sidebar', function() {
+    var link = function(scope, element, attrs) {
+      scope.showSidebar = function() {
+        console.log('toggling showSidebar ...');
+        if (angular.element('aside').css('display') == 'none') {
+          angular.element('aside').css('display', 'block');
+          angular.element('.aside-open').css('display', 'none');
+        }
+      };
+
+      scope.hideSidebar = function() {
+        console.log('toggling hideSidebar ...');
+        if (angular.element('aside').css('display') == 'block') {
+          angular.element('aside').css('display', 'none');
+          angular.element('.aside-open').css('display', 'block');
+        }
+      };
+    };
+
     return {
       restrict: 'A',
-      templateUrl: '../js/views/sidebar.html'
+      link: link,
+      templateUrl: function(elem, attr) {
+        return '../js/views/sidebar.html';
+      }
     };
   });
 
