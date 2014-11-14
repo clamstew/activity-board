97e39b19022656b0b881ffd3d5a9224d15235256
Clay Stewart <cstewart@rosettastone.com>
2014-11-14 01:38:10 -0600

    Move header text to data vars

--- public/js/directives/abc-header.js
+++ public/js/directives/abc-header.js
@@ -3,8 +3,13 @@
 (function(app) {
 
   app.directive('abcHeader', function() {
+    var link = function(scope){
+      scope.title = 'Always Be Committing';
+      scope.subTitle = 'Monitor Open Source Github Projects in Real Time ';
+    };
     return {
       restrict: 'A',
+      link: link,
       templateUrl: '../js/views/abc-header.html'
     }
   });
