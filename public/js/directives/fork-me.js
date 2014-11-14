578e84fb3de1bac74537abf8c4c516d50c14b277
Clay Stewart <cstewart@rosettastone.com>
2014-11-14 01:39:03 -0600

    Move fork me banner text/links to scope data vars

--- public/js/directives/fork-me.js
+++ public/js/directives/fork-me.js
@@ -3,8 +3,14 @@
 (function(app) {
 
   app.directive('forkMeBanner', function() {
+    var link = function(scope, element, attrs) {
+      scope.repoUrl = 'https://github.com/clamstew/activity-board/tree/rewrite-angular';
+      scope.imageUrl = 'https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png';
+    };
+
     return {
       restrict: 'A',
+      link: link,
       templateUrl: '../js/views/fork-me-banner.html'
     } 
   });
