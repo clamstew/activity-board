35f2419876c264791ac7a7d8ba2e2842db5ca383
Clay Stewart <cstewart@rosettastone.com>
2014-11-16 01:18:23 -0600

    Add addRepo function and move code over to right 2 spaces

--- public/js/directives/sidebar.js
+++ public/js/directives/sidebar.js
@@ -2,37 +2,37 @@
 
 (function(app) {
 
-  app.directive('sidebar', ['$window', function($window) {
-    var link = function(scope, element, attrs) {
+  app.directive('sidebar', ['$window', '$log',
+    function($window, $log) {
+      var link = function(scope, element, attrs) {
 
-      // initialize sidebar as closed
-      scope.sidebarOpen = false;
-      scope.toggleSidebar = function() {
-        scope.sidebarOpen = !scope.sidebarOpen;
+        // initialize sidebar as closed
+        scope.sidebarOpen = false;
+        scope.toggleSidebar = function() {
+          scope.sidebarOpen = !scope.sidebarOpen;
+        };
+
+        scope.sidebarHeight = function() {
+          return $window.innerHeight - 20 + 'px';
+        };
+
+        //@TODO - also make it so it listens to the window resize event
+        //        and resizes the height on that event as well as when it is revealed
+
+        scope.addRepo = function($event, userRepoName) {
+          $log.log('add repo fired', userRepoName);
+          
+        };
+
+        scope.removeRepo = function(repo) {};
+
       };
 
-      // initialize input box as hidden
-      scope.addRepoInputDisplay = false;
-      scope.showAddRepoInput = function() {
-        scope.addRepoInputDisplay = !scope.addRepoInputDisplay;
+      return {
+        restrict: 'A',
+        link: link,
+        templateUrl: '../js/views/sidebar.html'
       };
-
-      scope.sidebarHeight = function() {
-        // return $window
-        return $window.innerHeight - 20 + 'px';
-      };
-      //@TODO - also make it so it listens to the window resize event
-      //        and resizes the height on that event as well as when it is revealed
-
-      scope.removeRepo = function(repo) {};
-
-    };
-
-    return {
-      restrict: 'A',
-      link: link,
-      templateUrl: '../js/views/sidebar.html'
-    };
-  }]);
+    }]);
 
 })(activityBoard.app);
