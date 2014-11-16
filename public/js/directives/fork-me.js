437dab1532cbcdd5829a0089b04fc3549f8e899e
Clay Stewart <cstewart@rosettastone.com>
2014-11-15 23:32:37 -0600

    Add live updating to fork me banner

--- public/js/directives/fork-me.js
+++ public/js/directives/fork-me.js
@@ -2,10 +2,12 @@
 
 (function(app) {
 
-  app.directive('forkMeBanner', function() {
+  app.directive('forkMeBanner', ['userSettings', function(userSettings) {
     var link = function(scope, element, attrs) {
       scope.repoUrl = 'https://github.com/clamstew/activity-board/tree/rewrite-angular';
       scope.imageUrl = 'https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png';
+
+      scope.showForkMeBanner = userSettings.showForkMeBanner;
     };
 
     return {
@@ -13,6 +15,6 @@
       link: link,
       templateUrl: '../js/views/fork-me-banner.html'
     } 
-  });
+  }]);
 
 })(activityBoard.app);
