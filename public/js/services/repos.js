d909164653ce80091b5306b358836780b294e86d
Clay Stewart <cstewart@rosettastone.com>
2014-11-16 01:16:31 -0600

    Move none public functions in repos.js to _private object and within that move all functions only used inside project to inside _ which is nested in private and only call private functions that are also in private with this.

--- public/js/services/repos.js
+++ public/js/services/repos.js
@@ -5,7 +5,55 @@
   app.service('repos', ['initialRepos', '$http', '$log',
     function(initialRepos, $http, $log) {
     // return initialRepos
-    var o = {};
+    var o = {},
+        _private = {
+          makeUsernameAndLink: function(commitDiffs) {
+            var self = this;
+            _.each(commitDiffs, function(commit){
+              if (commit.author !== null) {
+                commit.ghUsername = commit.author.login;
+                commit.ghProfileLink = commit.author.html_url;
+              } else {
+                commit.ghUsername = commit.commit.author.name;
+                commit.ghProfileLink = 'http://github.com';
+              }
+              commit = self._.makeDates(commit);
+            });
+            return commitDiffs;
+          },
+          makeUserLink: function(repo) {
+            var link = "http://github.com/" + repo.user;
+            return link;
+          },
+          makeRepoLink: function(repo) {
+            var link = "http://github.com/" + repo.user + "/" + repo.repo;
+            return link;
+          },
+          _: {
+            makeDates: function(commit) {
+              var currentdatetimeMinusTwo = new Date().minusHours(2)
+                , currentdatetimeMinusDay = new Date().minusHours(24)
+                , date = new Date(commit.commit.author.date)
+              ;
+              commit.recent = this.createClass(date, currentdatetimeMinusTwo, currentdatetimeMinusDay);
+              commit.date = date.toLocaleDateString() + " | " + date.toLocaleTimeString();
+              return commit;
+            },
+            createClass: function(date, currentdatetimeMinusTwo, currentdatetimeMinusDay) {
+              // add classes for how recent the commit was
+              var recent;
+              if ( date > currentdatetimeMinusTwo ) {
+                  recent = "recent";
+              } else if ( date > currentdatetimeMinusDay && date < currentdatetimeMinusTwo ) {
+                  recent = "past-day";
+              } else {
+                  recent = "";
+              }
+              return recent;
+            }
+          }
+        }
+    ;
 
     // to store all the commit log messages as array of arrays of objects
     o.repos = [];
@@ -14,57 +62,24 @@
       // don't run this if there are already repos
       // could make a update function and only use this on init
       if (this.repos.length) return;
-      var createClass = function(date, currentdatetimeMinusTwo, currentdatetimeMinusDay) {
-        // add classes for how recent the commit was
-        var recent;
-        if ( date > currentdatetimeMinusTwo ) {
-            recent = "recent";
-        } else if ( date > currentdatetimeMinusDay && date < currentdatetimeMinusTwo ) {
-            recent = "past-day";
-        } else {
-            recent = "";
-        }
-        return recent;
-      };
-      var makeUsernameAndLink = function(commitDiffs) {
-        _.each(commitDiffs, function(commit){
-          if (commit.author !== null) {
-            commit.ghUsername = commit.author.login;
-            commit.ghProfileLink = commit.author.html_url;
-          } else {
-            commit.ghUsername = commit.commit.author.name;
-            commit.ghProfileLink = 'http://github.com';
-          }
-          commit = makeDates(commit);
-        });
-        return commitDiffs;
-      };
-      var makeDates = function(commit) {
-        var currentdatetimeMinusTwo = new Date().minusHours(2);
-        var currentdatetimeMinusDay = new Date().minusHours(24);
-        var date = new Date(commit.commit.author.date);
-        commit.recent = createClass(date, currentdatetimeMinusTwo, currentdatetimeMinusDay);
-        commit.date = date.toLocaleDateString() + " | " + date.toLocaleTimeString();
-        return commit;
-      };
-      var makeUserLink = function(repo) {
-        var link = "http://github.com/" + repo.user;
-        return link;
-      };
-      var makeRepoLink = function(repo) {
-        var link = "http://github.com/" + repo.user + "/" + repo.repo;
-        return link;
-      };
-
-      if(initialRepos && initialRepos.length) {
+      if (initialRepos && initialRepos.length) {
         _.each(initialRepos, function(repo) {
           var repoUrl = "https://api.github.com/repos/" + repo.user + "/" + repo.repo + "/commits";
           $http.get(repoUrl).
             success(function(data, status, headers, config) {
-              data = makeUsernameAndLink(data);
+              // @TODO: research if I can leave this step out and
+              //        instead use the main objects username, repo to show up
+              //        in all the commits that are nest below it in the dom
+              //        you would think that would work...
+              // make user name and prifle link on all the commit diff
+              data = _private.makeUsernameAndLink(data);
+              // assign all diffs data to this repo.commets
               repo.commits = data;
-              repo.userLink = makeUserLink(repo);
-              repo.repoLink = makeRepoLink(repo);
+              // add username adn repos links to the repo object itself
+              // which contins the commits object
+              // use this to make the header on each blue repo box
+              repo.userLink = _private.makeUserLink(repo);
+              repo.repoLink = _private.makeRepoLink(repo);
               o.repos.push(repo);
             }).
             error(function(data, status, headers, config) {
@@ -74,6 +89,15 @@
         });
       }
       // else { // see if any repos are stored in local storage // otherwise init to empty array of repos}
+    };
+
+    o.add = function(userRepoName) {
+      var seperateUserAndRepoName = function(userRepoName) {
+        // separate on the slash into array
+        // return array;
+      };
+
+
     };
 
     // remove a repo from the this.repos object
